d926ca9bc568582a9e56d6d9e16c6328f75fa983
Simon Duquennoy <simonduq@sics.se>
2013-06-26 13:56:59 +0200

    Make DS6 neighbors use neighbor-table, and move all DS6 neighbor management from uip-ds6 to a new uip-ds6-neighbor module.

--- core/net/tcpip.c
+++ core/net/tcpip.c
@@ -567,6 +567,7 @@ tcpip_ipv6_output(void)
       uip_ds6_route_t* locrt;
       locrt = uip_ds6_route_lookup(&UIP_IP_BUF->destipaddr);
       if(locrt == NULL) {
+        PRINTF("tcpip_ipv6_output: no route found, using default route\n");
         if((nexthop = uip_ds6_defrt_choose()) == NULL) {
 #ifdef UIP_FALLBACK_INTERFACE
 	  PRINTF("FALLBACK: removing ext hdrs & setting proto %d %d\n", 
@@ -588,11 +589,14 @@ tcpip_ipv6_output(void)
       } else {
 	nexthop = &locrt->nexthop;
       }
-#if TCPIP_CONF_ANNOTATE_TRANSMISSIONS
       if(nexthop != NULL) {
+        PRINTF("tcpip_ipv6_output: next hop ");
+        PRINT6ADDR(nexthop);
+        PRINTF("\n");
+#if TCPIP_CONF_ANNOTATE_TRANSMISSIONS
 	printf("#L %u 1; red\n", nexthop->u8[sizeof(uip_ipaddr_t) - 1]);
-      }
 #endif /* TCPIP_CONF_ANNOTATE_TRANSMISSIONS */
+      }
     }
     /* End of next hop determination */
 #if UIP_CONF_IPV6_RPL
@@ -651,7 +655,7 @@ tcpip_ipv6_output(void)
         PRINTF("tcpip_ipv6_output: nbr cache entry stale moving to delay\n");
       }
 
-      tcpip_output(&nbr->lladdr);
+      tcpip_output(uip_ds6_nbr_get_ll(nbr));
 
 #if UIP_CONF_IPV6_QUEUE_PKT
       /*
